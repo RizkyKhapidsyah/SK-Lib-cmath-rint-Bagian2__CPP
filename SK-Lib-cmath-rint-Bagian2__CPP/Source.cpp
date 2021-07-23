a73ce11d24258b2eac9ee1df5db694514a6deb27
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-23 20:58:44 +0700

    Add project files.

---
+++ SK-Lib-cmath-rint-Bagian2__CPP/Source.cpp
@@ -0,0 +1,19 @@
+
+#include <cmath>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	double a, b, x = 2.4, y = 2.9;
+
+	a = rint(x);
+	b = rint(y);
+
+	cout << a << endl;
+	cout << b;
+
+	_getch();
+	return 0;
+}
